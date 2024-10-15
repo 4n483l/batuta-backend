fe3035645b33fd350148002ee542d2073443cfad
4n483l <anabel.notar@gmail.com>
2024-10-15 17:15:59 +0200

    creacion roles

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('user_type', ['student', 'teacher', 'musician'])->default('student');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
