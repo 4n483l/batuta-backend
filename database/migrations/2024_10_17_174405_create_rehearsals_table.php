eca04536dcb02dec43d1d9915de6ce2017992bf2
4n483l <anabel.notar@gmail.com>
2024-10-17 20:40:59 +0200

    migraciones creadas

---
+++ database/migrations/2024_10_17_174405_create_rehearsals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRehearsalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    { // ensayos
+        Schema::create('rehearsals', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('hour');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rehearsals');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
